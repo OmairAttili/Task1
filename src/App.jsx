47f4a7d83e372de25c92155c9c89910f164c036b
OmairAttili <omairattili858@gmail.com>
2026-09-09 22:38:43 +0300

    Create Navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import Hero from './components/hero/Hero'
+import Portfolio from './components/portfolio/Portfolio'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+export default function App() {
+  return <>
+    <Navbar/>
+    <Hero/>
+    <Portfolio/>
+    <About/>
+    <Contact/>
+    <Footer/>
+  </>
+}
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+export default function BasicMenu() {
+  const id = React.useId();
+  const buttonId = `${id}-button`;
+  const menuId = `${id}-menu`;
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id={buttonId}
+        aria-controls={open ? menuId : undefined}
+        aria-haspopup="true"
+        aria-expanded={open}
+        onClick={handleClick}
+        sx={{bgcolor:'#1abc9c',color:'white',fontSize:13}}
+      >
+        Menu
+      </Button>
+      <Menu
+        id={menuId}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        slotProps={{
+          list: {
+            'aria-labelledby': buttonId,
+          },
+        }}
+      >
+        <MenuItem href='#' onClick={handleClose}>PORTFOLIO</MenuItem>
+        <MenuItem href='#' onClick={handleClose}>ABOUT</MenuItem>
+        <MenuItem href='#' onClick={handleClose}>CONTACT</MenuItem>
+      </Menu>
+    </div>
+  );
+}
