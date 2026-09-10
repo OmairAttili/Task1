59287aed998eea1f073bc2c53ceb0dc137a255e9
OmairAttili <omairattili858@gmail.com>
2026-09-10 14:54:24 +0300

    Create Lower footer

--- src/components/footer/LowerFooter.jsx
+++ src/components/footer/LowerFooter.jsx
@@ -1,7 +1,11 @@
+
+import { Box, Container, Typography} from '@mui/material'
 import React from 'react'
 
 export default function LowerFooter() {
-  return (
-    <div>LowerFooter</div>
-  )
+  return <Box sx={{bgcolor:'#1a252f',p:3}}>
+    <Container>
+      <Typography sx={{fontSize:14,color:'white',textAlign:'center',fontWeight:400}}>Copyright © Your Website 2023</Typography>
+    </Container>
+  </Box>
 }
